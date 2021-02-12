a0eedf2d7b15ef70651ee80a3e69b3cefc7c409c
pjblitz86 <paulius.juknis@gmail.com>
2021-02-12 10:54:01 +0200

    doubleMoney btn - map function

--- script.js
+++ script.js
@@ -44,4 +44,12 @@ function formatMoney(number) {
   return '$' + number.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');
 }
 
+function doubleMoney() {
+  data = data.map((user) => {
+    return { ...user, money: user.money * 2 };
+  });
+  updateDOM();
+}
+
 addUserBtn.addEventListener('click', getRandomUser);
+doubleBtn.addEventListener('click', doubleMoney);
